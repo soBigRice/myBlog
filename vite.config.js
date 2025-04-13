087bb0374c1a02cf37ccd45b7e5b4f210aaff3d5
soBigRice <soBigRice@users.noreply.github.com>
2025-04-13 11:04:59 +0800

    feat：更新区分开发环境和生产环境

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,34 @@
  * @Author: superRice
  * @Date: 2025-04-11 11:20:15
  * @LastEditors: soBigRice soBigRice@users.noreply.github.com
- * @LastEditTime: 2025-04-13 10:50:34
+ * @LastEditTime: 2025-04-13 11:03:02
  * @FilePath: /satelliteForRice/vite.config.js
  * @Description:
  * Do your best to be yourself
  * Copyright (c) 2025 by superRice, All Rights Reserved.
  */
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // 注意：要引入 path
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-  },
-  base: "/myBlog/",
-  resolve: {
-    alias: {
-      "/public": path.resolve(__dirname, "./myBlog"),
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: "0.0.0.0",
+//   },
+//   base: "/myBlog/",
+//   resolve: {},
+// });
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
     },
-  },
+    base: env.VITE_APP_BASE_URL || "/",
+    resolve: {},
+  };
 });
